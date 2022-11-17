0d3da5bef73460c99b73815fae6c24e983faf276
Khrystyna <hrustunakopeika2005@gmail.com>
2022-11-17 15:36:33 +0200

    first master commit

---
+++ лаба 6.3.1.cpp
@@ -0,0 +1,42 @@
+﻿// лаба 6.3.1.cpp 
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int* b, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		b[i] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int* b, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << b[i];
+	cout << endl;
+}
+
+
+int Sum(int* b, const int size)
+{
+	int s = 0;
+	for (int i = 0; i < size; i++)
+		if (b[i] < 0) s += b[i];
+	return s;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 24;
+	int b[n];
+	int Low = -15;
+	int High = 75;
+	Create(b, n, Low, High);
+	Print(b, n);
+	cout << Sum(b, n);
+	return 0;
+}
---
+++ юніт тест 6.3.1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 6.3.1/лаба 6.3.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест631
+{
+	TEST_CLASS(юніттест631)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int s[1] = { 1 };
+			int b;
+			b = Sum(s, 0);
+			Assert::AreEqual(b, 0);
+		}
+	};
+}
